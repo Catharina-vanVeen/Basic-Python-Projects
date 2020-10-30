931c36eddcd4e655c22ffb74a92c90fffd24bd1a
Maria Catharina van Veen <catharina.vanVeen@Libertyserver.com>
2020-10-30 15:29:38 -0400

    Django University Complete & Django checkbook setup

---
+++ Django_Checkbook/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\micro\documents\github catharina\python-projects\django_checkbook\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
