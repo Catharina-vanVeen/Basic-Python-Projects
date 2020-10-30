a8507892d99cb9dbd693772089c660c3566e5e2a
Maria Catharina van Veen <catharina.vanVeen@Libertyserver.com>
2020-10-30 15:56:20 -0400

    Django Check book set up

---
+++ Django_Checkbook/src/mainApp/accounts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models import *
+import datetime
+from datetime import date
+
+# Create your models here.
+class Account(models.Model):
+    firstName = models.CharField(max_length=60, blank=False, null=False)
+    lastName = models.CharField(max_length=60, blank=False, null=False)
+    startingBalance = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False, default=0.00)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return "{:09}\t{}, {}".format(self.pk, self.lastName, self.firstName)
+
+TYPE_CHOICES = {
+    ('D', 'deposit'),
+    ('W', 'withdrawl'),
+}
+
+class Transaction(models.Model):
+    date = DateField(auto_now=False, auto_now_add=False, blank=False, null=False, default=datetime.date.today)
+    type =  models.CharField(max_length=20, blank=False, null=False, choices=TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
+    description = models.CharField(max_length=255, blank=True, null=False)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return "{:09}\t{}\t{}".format(self.pk, self.date, self.account)
